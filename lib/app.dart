0e187bbca57bf06a83003825615f31ab609d2364
Waseem-Gopang <wasemgopaang@gmail.com>
2022-01-22 17:45:20 +0500

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'login.dart';
+
+// TODO: Convert ShrineApp to stateful widget (104)
+class ShrineApp extends StatelessWidget {
+  const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shrine',
+      // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
+      home: const HomePage(),
+      // TODO: Make currentCategory field take _currentCategory (104)
+      // TODO: Pass _currentCategory for frontLayer (104)
+      // TODO: Change backLayer field value to CategoryMenuPage (104)
+      initialRoute: '/login',
+      onGenerateRoute: _getRoute,
+      // TODO: Add a theme (103)
+    );
+  }
+
+  Route<dynamic>? _getRoute(RouteSettings settings) {
+    if (settings.name != '/login') {
+      return null;
+    }
+
+    return MaterialPageRoute<void>(
+      settings: settings,
+      builder: (BuildContext context) => const LoginPage(),
+      fullscreenDialog: true,
+    );
+  }
+}
+
+// TODO: Build a Shrine Theme (103)
+// TODO: Build a Shrine Text Theme (103)
---
+++ lib/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // TODO: Make a collection of cards (102)
+  // TODO: Add a variable for Category (104)
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return const Scaffold(
+      // TODO: Add app bar (102)
+      // TODO: Add a grid view (102)
+      body: Center(
+        child: Text('You did it!'),
+      ),
+      resizeToAvoidBottomInset: false,
+    );
+  }
+}
